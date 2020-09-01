446c51b204ed710de36621209ea9c7fc9fbee197
unknown <you@example.com>
2020-09-01 02:25:18 -0700

    post and users route updated

--- cinema/posts/routes.py
+++ cinema/posts/routes.py
@@ -3,14 +3,22 @@ from flask import (render_template, url_for, flash,
 
 from flask_login import login_required, current_user
 #from cinema.models import Movie
+from cinema import db
+from cinema.models import Movie
 from cinema.posts.forms import MoviePostForm
 
 movies = Blueprint('movies', __name__)
 
-@movies.route("/movies/new", methods=['GET', 'POST'])
-@login_required #? Admins only
-def new_movie():
-    form = MoviePostForm()
 
-    return 
-    
+@movies.route("/movices/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form =MoviePostForm()
+    if form.validate_on_submit():
+        post = Movie(title = form.title.data ,cover_image =form.cover_image.data,rating =form.rating.data, trailer =form.trailer.data,genres=form.genres.data,
+                     language =form.language.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
--- cinema/users/routes.py
+++ cinema/users/routes.py
@@ -1,28 +1,26 @@
 # Import others when you use them
 from flask import Blueprint
 from flask import render_template, url_for, flash, redirect, request
-from cinema.users import db, app
+import cinema
+from cinema.users import db
 from cinema.users.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from ..models import Movie, User, Role, Cinema
+from ..models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
 users = Blueprint('users', __name__)
-
-@users.route("/")
-@users.route("/home")
-def home():
-    return render_template("home.html")
 
 
 @users.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, gender=form.gender.data,
-                    password=form.password.data)
-        db.session.add(User)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
+        username = User(username=form.username.data, email=form.email.data, gender=form.gender.data)
+
+        password = User(password=form.password.data)
+        db.add(password)
+        db.commit()
+
+        flash('Your account has been created!, wait till you get the confirmation code')
         return redirect(url_for('login'))
         return render_template('register.html', form=form)
 
@@ -43,12 +41,30 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@users.route("/developer", methods=['GET', 'POST'])
+def developer():
+    developer_mssg = register.username
+    db.session.add(developer_mssg)
+    db.commit()
+    flash("you added a user!")
+    return render_template('developer.html', title='developer')
+
+
 @users.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-
+        if form.picture.data:
+            from cinema.users.utils import save_picture
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -57,8 +73,6 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-
-    return render_template('account.html', title='Account', form=form)
-
-
-
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
