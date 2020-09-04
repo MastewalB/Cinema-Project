56714374afa1c52c0c42cd7eae3a11102941a190
Mastewal B <se.mastewal.belete@gmail.com>
2020-09-04 11:33:24 -0200

    Autopep8

--- cinema/posts/forms.py
+++ cinema/posts/forms.py
@@ -3,34 +3,41 @@ from wtforms import DateTimeField, StringField, SubmitField, TextAreaField, Deci
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
 
-#this form is used to add a new Movie
-#? How to add movie trailer and rating?
-LANGUAGE_CHOICES = ['English','Amharic'] # This is for sample will be re-arranged
-#Class not finished
+# this form is used to add a new Movie
+# ? How to add movie trailer and rating?
+# This is for sample will be re-arranged
+LANGUAGE_CHOICES = ['English', 'Amharic']
+# Class not finished
+
+
 class MoviePostForm(FlaskForm):
     movie_title = StringField('Movie Title', validators=[DataRequired()])
     cover_image = FileField('Enter Movie Cover',
-                            validators=[FileAllowed(['jpg','png'])])
+                            validators=[FileAllowed(['jpg', 'png'])])
     genres = StringField('Genres', validators=[DataRequired()])
     trailer = StringField('Trailer')
     plot = TextAreaField('Plot')
     language = SelectField('Language', choices=LANGUAGE_CHOICES)
-    
+
     submit = SubmitField('Add')
-    
+
 
 class EditMovieForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
-    cover_image = FileField( 'Cover Image', validators=[FileAllowed(['jpg','png'])] )
+    cover_image = FileField('Cover Image', validators=[
+                            FileAllowed(['jpg', 'png'])])
     rating = DecimalField('rating', places=1, validators=[DataRequired()])
-    
-    trailer = FileField( 'movie trailer', validators=[FileAllowed(['mp4','mkv'])] )
+
+    trailer = FileField('movie trailer', validators=[
+                        FileAllowed(['mp4', 'mkv'])])
     genres = StringField('genres', validators=[DataRequired()])
     language = StringField('language', validators=[DataRequired()])
     plot = TextAreaField('plot', validators=[DataRequired()])
 
     submit = SubmitField('Save Edit')
 
+
 class ScheduleForm(FlaskForm):
-    schedule = DateTimeField( 'Pick a Date', format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()] )
+    schedule = DateTimeField(
+        'Pick a Date', format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()])
     submit = SubmitField("Save Schedule")
