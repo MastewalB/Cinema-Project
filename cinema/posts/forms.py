2b0dbc69c48cd1423900e751aa8f75e84848348a
Mastewal B <se.mastewal.belete@gmail.com>
2020-09-03 23:10:09 -0200

    Schedule Form

--- cinema/posts/forms.py
+++ cinema/posts/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, DecimalField, IntegerField, SelectField
+from wtforms import DateTimeField, StringField, SubmitField, TextAreaField, DecimalField, IntegerField, SelectField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired
 
@@ -10,7 +10,7 @@ LANGUAGE_CHOICES = ['English','Amharic'] # This is for sample will be re-arrange
 class MoviePostForm(FlaskForm):
     movie_title = StringField('Movie Title', validators=[DataRequired()])
     cover_image = FileField('Enter Movie Cover',
-                             validators=[FileAllowed(['jpg','png'])])
+                            validators=[FileAllowed(['jpg','png'])])
     genres = StringField('Genres', validators=[DataRequired()])
     trailer = StringField('Trailer')
     plot = TextAreaField('Plot')
@@ -21,9 +21,15 @@ class MoviePostForm(FlaskForm):
 class EditMovieForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     cover_image = FileField( 'Cover Image', validators=[FileAllowed(['jpg','png'])] )
-    rating = DecimalField('rating', validators=[DataRequired()])
+    rating = DecimalField('rating', places=1, validators=[DataRequired()])
     
     trailer = FileField( 'movie trailer', validators=[FileAllowed(['mp4','mkv'])] )
     genres = StringField('genres', validators=[DataRequired()])
     language = StringField('language', validators=[DataRequired()])
     plot = TextAreaField('plot', validators=[DataRequired()])
+
+    submit = SubmitField('Save Edit')
+
+class ScheduleForm(FlaskForm):
+    schedule = DateTimeField( 'Pick a Date', format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()] )
+    submit = SubmitField("Save Schedule")
--- cinema/posts/routes.py
+++ cinema/posts/routes.py
@@ -2,18 +2,17 @@ from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 
 from flask_login import login_required, current_user
-#from cinema.models import Movie
 from cinema import db
 from cinema.models import Movie
-from cinema.posts.forms import MoviePostForm
+from cinema.posts.forms import MoviePostForm, ScheduleForm, EditMovieForm
 
 movies = Blueprint('movies', __name__)
 
 
-@movies.route("/movices/new", methods=['GET', 'POST'])
+@movies.route("/movies/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
-    form =MoviePostForm()
+    form = MoviePostForm()
     if form.validate_on_submit():
         post = Movie(title = form.title.data ,cover_image =form.cover_image.data,rating =form.rating.data, trailer =form.trailer.data,genres=form.genres.data,
                      language =form.language.data)
@@ -22,3 +21,12 @@ def new_post():
         flash('Your post has been created!', 'success')
         return redirect(url_for('main.home'))
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@movies.route("/new_schedule", methods=['GET', 'POST'])
+def new_schedule():
+    form = ScheduleForm()
+    if form.validate_on_submit():
+        #schedule will be added to database
+        flash("Schedule has been Added.")
+        return render_template('new_schedule.html', form=form)
